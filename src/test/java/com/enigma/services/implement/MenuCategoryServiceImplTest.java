a046fbcd053333629e6d431b3f8e25e28aad6869
Aldilaiskandar <aldila.iskndr@gmail.com>
2019-11-06 22:33:34 +0700

    add testing for MenuServiceImpl

---
+++ src/test/java/com/enigma/services/implement/MenuCategoryServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.enigma.services.implement;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuCategoryServiceImplTest {
+
+    @Test
+    public void getAllMenuCategory() {
+    }
+
+    @Test
+    public void createMenuCategory() {
+    }
+
+    @Test
+    public void deleteMenuCategoryById() {
+    }
+
+    @Test
+    public void updateMenuCategory() {
+    }
+}

ff3dc06b901d84e1e39c187ea9dc1faa6ee46010
Aldilaiskandar <aldila.iskndr@gmail.com>
2019-11-06 20:59:16 +0700

    update MenuCategory

--- src/main/java/com/enigma/controllers/MenuCategoryController.java
+++ src/main/java/com/enigma/controllers/MenuCategoryController.java
@@ -1,7 +1,35 @@
 package com.enigma.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.enigma.entities.MenuCategory;
+import com.enigma.services.MenuCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@CrossOrigin
 public class MenuCategoryController {
+
+    @Autowired
+    MenuCategoryService menuCategoryService;
+
+    @GetMapping("/all-menu-category")
+    public List<MenuCategory> getAllMenuCategory(){
+        return menuCategoryService.getAllMenuCategory();
+    }
+
+    @PostMapping("/create/menu-category")
+    public MenuCategory createMenuCategory(@RequestBody MenuCategory menuCategory){
+        return menuCategoryService.createMenuCategory(menuCategory);
+    }
+    @DeleteMapping("/delete/menu-category/{id}")
+    public void deleteMenuCategoryById(@PathVariable String id){
+        menuCategoryService.deleteMenuCategoryById(id);
+    }
+
+    @PutMapping("update/menu-category")
+    public MenuCategory updateMenuCategory(@RequestBody MenuCategory menuCategory){
+        return menuCategoryService.updateMenuCategory(menuCategory);
+    }
 }
--- src/main/java/com/enigma/controllers/MenuController.java
+++ src/main/java/com/enigma/controllers/MenuController.java
@@ -1,11 +1,53 @@
 package com.enigma.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.enigma.entities.Menu;
+import com.enigma.services.MenuService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class MenuController {
-    
+    @Autowired
+    MenuService menuService;
+
+
+    @PostMapping("/menu")
+    public Menu createMenu(@RequestBody Menu menu){
+        return menuService.createMenu(menu);
+    }
+
+    @PostMapping("/menu/upload")
+    public Menu createMenuWithImage(@RequestPart MultipartFile image, String menuInput) throws IOException {
+        return menuService.createMenuWithImage(menuInput, image);
+    }
+
+    @GetMapping("/menu/{id}")
+    public Menu getMenuById(@PathVariable String id){
+        return menuService.getMenuById(id);
+    }
+
+    @GetMapping("/menuList")
+    public List<Menu> getAllMenu(){
+        return menuService.getAllMenu();
+    }
+
+    @DeleteMapping("delete/menu/{id}")
+    public void deleteMenu(@PathVariable String id){
+        menuService.deleteMenuById(id);
+    }
+
+    @PutMapping("/update/menu")
+    public Menu updateMenu(@RequestBody Menu menu){
+        return menuService.updateMenu(menu);
+    }
+
 }

ba0bc0a3e863e3aec267d933953f9fa3b08ff58b
Aldilaiskandar <aldila.iskndr@gmail.com>
2019-11-06 18:05:31 +0700

    initial

---
+++ src/main/java/com/enigma/WmbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmbBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmbBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enigma/WmbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmbBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

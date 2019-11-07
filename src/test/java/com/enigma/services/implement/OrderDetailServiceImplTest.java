56fc12bd714cdd584aeb4232c7de3e454770cad0
Tn Martin Jham <academyyyy12@gmail.com>
2019-11-07 10:42:31 +0700

    add testing for DetailOrderServiceImpl

---
+++ src/test/java/com/enigma/services/implement/OrderDetailServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.enigma.services.implement;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OrderDetailServiceImplTest {
+
+    @Test
+    public void getOrderDetailById() {
+    }
+
+    @Test
+    public void getAllOrderDetail() {
+    }
+
+    @Test
+    public void createOrderDetail() {
+    }
+
+    @Test
+    public void deleteOrderDetailById() {
+    }
+
+    @Test
+    public void updateOrderDetail() {
+    }
+}

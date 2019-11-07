031ef4b1d334f6ea9877fe3e7e0f6d86ed0e215c
Aldilaiskandar <aldila.iskndr@gmail.com>
2019-11-07 09:04:24 +0700

    add exception input category

--- src/main/java/com/enigma/exceptions/InputCanNotBeEmptyException.java
+++ src/main/java/com/enigma/exceptions/InputCanNotBeEmptyException.java
@@ -1,4 +1,15 @@
 package com.enigma.exceptions;
 
-public class InputCanNotBeEmptyException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InputCanNotBeEmptyException extends RuntimeException {
+    public InputCanNotBeEmptyException(String message) {
+        super(message);
+    }
+
+    public InputCanNotBeEmptyException() {
+        super("Sorry, data have to Exist");
+    }
 }

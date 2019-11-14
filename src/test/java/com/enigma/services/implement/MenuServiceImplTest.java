3b8232dd2ba5bbf1bb1f4dbbdc5921968e6b4d21
whydn.id <whydn.id@hotmail.com>
2019-11-14 22:29:46 +0700

    Updated unit test

--- src/test/java/com/enigma/services/implement/MenuServiceImplTest.java
+++ src/test/java/com/enigma/services/implement/MenuServiceImplTest.java
@@ -52,7 +52,7 @@ public class MenuServiceImplTest {
     public void getMenuById_should_return_Menu_when_Found() {
         menuCategory = menuCategoryService.createMenuCategory(menuCategory);
         Menu menu = new Menu("Ikan Bakar", new BigDecimal(50000), true, menuCategory.getIdMenuCategory() );
-        menu = menuService.createMenu(menu);
+        menu = menuRepository.save(menu);
         Menu actual = menuService.getMenuById(menu.getIdMenu());
         Menu expected = menuRepository.findById(menu.getIdMenu()).get();
         assertEquals(expected, actual);
@@ -62,7 +62,7 @@ public class MenuServiceImplTest {
     public void getMenuById_should_return_notnull_when_Found(){
         menuCategory = menuCategoryService.createMenuCategory(menuCategory);
         Menu menu = new Menu("Ikan Bakar", new BigDecimal(50000), true, menuCategory.getIdMenuCategory() );
-        menu = menuService.createMenu(menu);
+        menu = menuRepository.save(menu);
         assertNotEquals(0,menuService.getMenuById(menu.getIdMenu()));
     }
 
@@ -71,19 +71,11 @@ public class MenuServiceImplTest {
         menuCategory = menuCategoryService.createMenuCategory(menuCategory);
         Menu menu1 = new Menu("Ikan Bakar", new BigDecimal(50000), true, menuCategory.getIdMenuCategory());
         Menu menu2 = new Menu("Ayam Bakar", new BigDecimal(40000), true, menuCategory.getIdMenuCategory());
-        menuService.createMenu(menu1);
-        menuService.createMenu(menu2);
+        menuRepository.save(menu1);
+        menuRepository.save(menu2);
         assertEquals(2, menuService.getAllMenu().size());
     }
 
-    @Test
-    public void createMenu_should_return_Menu_when_inputMenu() {
-        menuCategory = menuCategoryService.createMenuCategory(menuCategory);
-        Menu menu = new Menu("Ikan Bakar", new BigDecimal(50000), true, menuCategory.getIdMenuCategory());
-        menu = menuService.createMenu(menu);
-        Menu expected = menuRepository.findById(menu.getIdMenu()).get();
-        assertEquals(expected, menu);
-    }
 
     @Test
     public void createMenuWithImage_should_return_menu_and_image_ExistOnServer() throws IOException {
@@ -105,20 +97,10 @@ public class MenuServiceImplTest {
         menuCategory = menuCategoryService.createMenuCategory(menuCategory);
         Menu menu1 = new Menu("Ikan Bakar", new BigDecimal(50000), true, menuCategory.getIdMenuCategory());
         Menu menu2 = new Menu("Ayam Bakar", new BigDecimal(40000), true, menuCategory.getIdMenuCategory());
-        menu1 = menuService.createMenu(menu1);
-        menuService.createMenu(menu2);
+        menu1 = menuRepository.save(menu1);
+        menuRepository.save(menu2);
         menuService.deleteMenuById(menu1.getIdMenu());
         assertEquals(1, menuService.getAllMenu().size());
     }
 
-    @Test
-    public void updateMenu_should_return_new_Menu_when_Data_Updated() {
-        menuCategory = menuCategoryService.createMenuCategory(menuCategory);
-        Menu menu = new Menu("Ikan Bakar", new BigDecimal(50000), true, menuCategory.getIdMenuCategory());
-        menu = menuService.createMenu(menu);
-        Menu edited = menu;
-        edited.setMenuName("Ayam Bakar");
-        menuService.updateMenu(edited);
-        assertEquals(edited, menuRepository.findById(menu.getIdMenu()).get());
-    }
 }
